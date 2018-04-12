a91144b12d3d62bbb168b235b95e85e1a2f658ae
Tim Cheung <tim@cheung.io>
2018-04-12 17:41:37 +0200

    make it work

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+#!/usr/bin/env node
+'use strict';
 
+const meow = require('meow')
+const ora = require('ora')
+const hannoverjs = require('hannoverjs')
+
+const cli = meow(`
+  Get next HannoverJS meetup date.
+
+  Usage
+    $ hannoverjs
+`)
+
+const spinner = ora('Fetching next meetup').start()
+
+hannoverjs.getEvents().then(({ body }) => {
+  spinner.stop()
+
+  const [nextMeetup] = body
+
+  if (!nextMeetup) {
+    return console.log(`The next meetup has no date yet 😢`)
+  }
+
+  const date = new Date(nextMeetup.date).toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric'
+  })
+
+  console.log(`Next meetup is on ${date} 🚀`)
+})
